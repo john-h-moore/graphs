b46303216c3b8f3c11bb8865d8ce1f1697d585e8
john-h-moore <john@jhmwebdesign.com>
2013-05-24 13:22:33 -0400

    Added dfs_dest() to search.py
    Perform dfs search terminating at destination node

--- search.py
+++ search.py
@@ -19,3 +19,15 @@ def bfs(G, v, marked):
 				marked[neighbor] = True
 				queue.append(neighbor)
 	return marked
+
+def dfs_dest(G, start, end, marked, explored):
+	marked[start] = True
+	for neighbor in G.nodes[start].neighbors:
+		if G.nodes[start].neighbors[neighbor] not in explored:
+			if neighbor not in marked:
+				explored.append(G.nodes[start].neighbors[neighbor][0])
+				if neighbor == end:
+					marked[end] = True
+					return marked, explored
+				dfs_dest(G, neighbor, end, marked, explored)
+	return None
